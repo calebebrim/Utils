d2a59590c98ba5e05adf3b80de4c557a31431f05
Calebe E. R. Brim <calebebrim@gmail.com>
2018-10-22 22:54:40 -0300

    unix based scripts

---
+++ scripts/sh/unix_based/ipfs.sh
@@ -0,0 +1,10 @@
+
+
+# ipfs commands 
+# add folder recursivelly
+ipfs add -r <folder> # pin it 
+ipfs pin add -r <hash> # pin it 
+
+# remove folder
+ipfs pin rm -r <hash> # pin it 
+ipfs repo gc
---
+++ scripts/sh/unix_based/neo4j.sh
@@ -0,0 +1,1 @@
+run --publish=7474:7474 --publish=7687:7687 --volume=/Users/calebebrim/Documents/neo4j/data:/data neo4j
